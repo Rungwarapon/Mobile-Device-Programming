20d5b9dcadcbef1c80bb738cd51b1775df1b4a6e
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-10-14 01:13:36 +0700

    YoYo

---
+++ Navigatamaru/screens/MealDetailScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+const MealDetailScreen = (props) => {
+  // เขียนโค้ดเพิ่ม เพื่อดึงอ็อบเจ๊คเมนูอาหารที่ผู้ใช้เลือกเอาไว้
+  const catId = props.navigation.getParam("deeId");
+  const MealTwo = MEALS.find((cat) => cat.id === catId);
+
+  return (
+    <View style={styles.screen}>
+      <Text>The Meal Detail Screen!</Text>
+      <Text>{MealTwo.title}</Text>
+      <Text>{MealTwo.steps}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
+    </View>
+  );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  // เขียนโค้ดเพิ่มเพื่อแสดงชื่อเมนูอาหารที่เลือกให้เป็นเฮดเดอร์
+  const catId = navigationData.navigation.getParam("deeId");
+  const MealTwo = MEALS.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: MealTwo.title
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MealDetailScreen;
